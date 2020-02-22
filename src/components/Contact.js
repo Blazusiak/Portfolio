ef3c34a25ee76ee5d8d441293aa46112efbf0bb8
Jacob <jblazusi@ualberta.ca>
2020-02-21 19:17:23 -0700

    Integrated emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import * as emailjs from "emailjs-com";
 import {
   MDBRow,
   MDBCol,
@@ -13,6 +14,72 @@ import {
 import contact from "../assets/contact.jpg";
 
 export class Contact extends Component {
+  state = {
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+    sent: false
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+    event.target.className += " was-validated";
+    let formValid = true;
+
+    // Check if fields are empty
+    if (
+      !this.state.name ||
+      !this.state.email ||
+      !this.state.subject ||
+      !this.state.message
+    ) {
+      formValid = false;
+    }
+
+    if (
+      this.state.name &&
+      this.state.email &&
+      this.state.subject &&
+      this.state.message
+    ) {
+      formValid = true;
+      this.resetForm();
+      event.target.className -= " was-validated";
+    }
+
+    const { name, email, subject, message } = this.state;
+    let templateParams = {
+      name: name,
+      to: "jblazusi@ualberta.ca",
+      email: email,
+      subject: subject,
+      message: message
+    };
+    if (formValid) {
+      emailjs.send(
+        "gmail",
+        "example",
+        templateParams,
+        "user_QOKDE7xgzlZNjAYvqugow"
+      );
+    }
+  };
+
+  changeHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  resetForm() {
+    this.setState({
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+      sent: true
+    });
+  }
+
   render() {
     return (
       <div className="pb-5 pt-5" id="contact">
@@ -23,23 +90,38 @@ export class Contact extends Component {
           <MDBCardBody>
             <MDBRow>
               <MDBCol md="4" className="md-0">
-                <form>
+                <form
+                  className="needs-validation"
+                  id="contact-form"
+                  onSubmit={this.submitHandler}
+                  noValidate
+                >
                   <MDBRow>
                     <MDBCol md="6">
                       <div className="md-form mb-0">
                         <MDBInput
                           type="text"
-                          id="contact-name"
+                          id="name"
                           label="Your name"
+                          name="name"
+                          value={this.state.name}
+                          onChange={this.changeHandler}
+                          className="form-control"
+                          required
                         />
                       </div>
                     </MDBCol>
                     <MDBCol md="6">
                       <div className="md-form mb-0">
                         <MDBInput
-                          type="text"
-                          id="contact-email"
+                          type="email"
+                          id="email"
                           label="Your email"
+                          name="email"
+                          value={this.state.email}
+                          onChange={this.changeHandler}
+                          className="form-control"
+                          required
                         />
                       </div>
                     </MDBCol>
@@ -49,8 +131,13 @@ export class Contact extends Component {
                       <div className="md-form mb-0">
                         <MDBInput
                           type="text"
-                          id="contact-subject"
+                          id="subject"
                           label="Subject"
+                          name="subject"
+                          value={this.state.subject}
+                          onChange={this.changeHandler}
+                          className="form-control"
+                          required
                         />
                       </div>
                     </MDBCol>
@@ -60,18 +147,29 @@ export class Contact extends Component {
                       <div className="md-form mb-0">
                         <MDBInput
                           type="textarea"
-                          id="contact-message"
+                          id="message"
                           label="Your message"
+                          name="message"
+                          value={this.state.message}
+                          onChange={this.changeHandler}
+                          className="form-control"
+                          required
                         />
                       </div>
                     </MDBCol>
                   </MDBRow>
+                  <div className="text-center text-md-left">
+                    {this.state.sent ? (
+                      <MDBBtn color="blue" type="submit" disabled>
+                        Thank you for your email!
+                      </MDBBtn>
+                    ) : (
+                      <MDBBtn color="blue" type="submit">
+                        Send
+                      </MDBBtn>
+                    )}
+                  </div>
                 </form>
-                <div className="text-center text-md-left">
-                  <MDBBtn color="primary" size="md">
-                    Send
-                  </MDBBtn>
-                </div>
               </MDBCol>
               <MDBCol md="3" className="text-center md-0 mb-5">
                 <ul className="list-unstyled mt-5">
